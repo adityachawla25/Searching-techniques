96946d94f8726cd48ae36b2e45cfd041c55bff69
Aditya Chawla <138515367+adityachawla25@users.noreply.github.com>
2023-09-14 20:10:56 +0530

    Update linearsearch.java

--- linearsearch.java
+++ linearsearch.java
@@ -1,28 +1,28 @@
-//int
-// public class linearsearch {
-//     public static int linearsearch(int numbers[],int key){
-//         for(int i =0;i<numbers.length;i++){
-//             if(numbers[i]==key){
-//                 return i;
-//             }
-//         }    
-//         return -1; 
-//     }    
-//     public static void main(String args[]){
-//         int numbers[]={13,16,4,5,6,7,67,32};
-//         int key=7;
+//int datatype
+public class linearsearch {
+    public static int linearsearch(int numbers[],int key){
+        for(int i =0;i<numbers.length;i++){
+            if(numbers[i]==key){
+                return i;
+            }
+        }    
+        return -1; 
+    }    
+    public static void main(String args[]){
+        int numbers[]={13,16,4,5,6,7,67,32};
+        int key=7;
 
-//         int index = linearsearch(numbers,key);
+        int index = linearsearch(numbers,key);
 
-//         if(index==-1){
-//             System.out.println("Not found");
-//         }else{
-//             System.out.println("The key is found at position " + index);
-//         }
-//     }
-// }
+        if(index==-1){
+            System.out.println("Not found");
+        }else{
+            System.out.println("The key is found at position " + index);
+        }
+    }
+}
 
-//String
+//String datatype
 public class linearsearch{
     public static int linearsearch(String menu[], String key){
         for(int i=1;i<menu.length;i++){
